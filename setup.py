21344e806f6746c320c77b7f5b7f1f35124fef5c
Michael von Bothmer <michael@bothmer.name>
2018-04-21 20:46:08 +0200

    new file:   .fehbg 	modified:   i3/config 	modified:   setup.py 	new file:   xlock/icon.png 	new file:   xlock/lock.sh

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python
 
 from os import symlink
+from shutil import move
 from os.path import expanduser as expand
 
 symlinks = {
     expand("~/dotfiles/.vim/"): expand("~/.vim"),
     expand("~/dotfiles/.vimrc"): expand("~/.vimrc"),
+    expand("~/dotfiles/.xprofile"): expand("~/.xprofile"),
+    expand("~/dotfiles/.fehbg"): expand("~/.fehbg"),
     expand("~/dotfiles/.zshrc"): expand("~/.zshrc"),
     expand("~/dotfiles/ranger"): expand("~/.config/ranger"),
     expand("~/dotfiles/termite"): expand("~/.config/termite"),
@@ -13,4 +16,9 @@ symlinks = {
 }
 
 for src, dst in symlinks.items():
+    try:
+        move(dst, dst + ".bak")
+    except FileNotFoundError:
+        pass
+
     symlink(src, dst)
